b73a3519dbb65185ee48e4d651429b671a728558
Parth Rajauria <parthrajauria@gmail.com>
2023-05-05 12:57:03 +0530

    EDA and Problems statement

--- src/exception.py
+++ src/exception.py
@@ -20,10 +20,11 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-if __name__ == "__main__":
+# To test this file
+# if __name__ == "__main__":
 
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info("Dividing by zero")
-        raise CustomException(e,sys) 
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         logging.info("Dividing by zero")
+#         raise CustomException(e,sys) 
